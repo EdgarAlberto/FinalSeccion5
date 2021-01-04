22d13867956d0d3707f175ff9f8c55645169abd0
EdgarAlberto <76929888+EdgarAlberto@users.noreply.github.com>
2021-01-03 21:50:16 -0600

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float maxTimeDog=1;
+    public float counter=1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        counter += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& counter>=maxTimeDog)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            counter = 0;
+        }
+        
+    }
+}
---
+++ SpawnManagerX.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    private int ball ;
+ 
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, nextWaitTime);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        //generar Random ball 
+        ball = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ball], spawnPos, ballPrefabs[ball].transform.rotation);
+    }
+
+
+    private float counter = 0f;
+    private float nextWaitTime = 5f;
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if(counter>=nextWaitTime)
+        {
+            //Generamos nueva oleada =nueva bola
+            Debug.LogFormat("Hemos esperado {0} segundos", nextWaitTime);
+            counter = 0;
+            nextWaitTime = Random.Range(2, 5);
+        }
+    }
+
+}
